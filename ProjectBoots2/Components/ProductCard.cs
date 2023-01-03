e7ef5b1fec066a7e0d19efd21e488829df010bcd
MadChipmunk03 <petaslachta@gmail.com>
2023-01-03 20:03:00 +0100

    checkpoint 1.3

---
+++ ProjectBoots2/Components/ProductCard.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectBoots2.Models;
+
+namespace ProjectBoots2.Components
+{
+    public class ProductCard : ViewComponent
+    {
+        public IViewComponentResult Invoke(Product product)
+        {
+            this.ViewBag.Product = product;
+            return View();
+        }
+    }
+}
--- ProjectBoots2/Controllers/HomeController.cs
+++ ProjectBoots2/Controllers/HomeController.cs
@@ -1,32 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProjectBoots2.Models;
+using ProjectBoots2.Models.contexts;
 using System.Diagnostics;
 
 namespace ProjectBoots2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        dbBootsContext context = new dbBootsContext();
 
-        public HomeController(ILogger<HomeController> logger)
+        public IActionResult Index(bool showAll)
         {
-            _logger = logger;
-        }
+            List<Product> products = context.Products.ToList();
+            products = showAll ? products : products.GetRange(0, 8);
 
-        public IActionResult Index()
-        {
+            List<ProductImage> images = context.ProductImages.ToList();
+
+            foreach (Product product in products)
+                product.ProductImages = images.Where(img => img.ProductId == product.Id).ToList();
+
+            ViewBag.Products = products;
+
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
