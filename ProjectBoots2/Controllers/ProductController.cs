dc915c989af349c88e154ee058c549b56383ecb7
MadChipmunk03 <petaslachta@gmail.com>
2023-01-09 19:22:15 +0100

    checkpoint 1.5

--- ProjectBoots2/Controllers/ProductController.cs
+++ ProjectBoots2/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjectBoots2.Models;
 using ProjectBoots2.Models.contexts;
 using System;
+using System.Collections.Generic;
 
 namespace ProjectBoots2.Controllers
 {
@@ -13,20 +15,55 @@ namespace ProjectBoots2.Controllers
         public IActionResult Index(int vrtId, int productId, int size, string color) =>
             vrtId == 0 ? IndexChangeVariation(productId, size, color) : IndexVariation(vrtId);
 
-        private IActionResult IndexVariation(int vrtId)
+        private void IndexBase(Variation variation)
         {
-            Variation variation = context.Variations.First(vrt => vrt.Id == vrtId);
             Product product = context.Products.First(prd => prd.Id == variation.ProductId);
             product.ProductImages = context.ProductImages.Where(img => img.ProductId == product.Id).ToList();
             List<Variation> variations = context.Variations.Where(vrt => vrt.ProductId == product.Id).ToList();
             int?[] sizes = variations.Select(vrt => vrt.Size).Distinct().ToArray();
             string?[] colors = variations.Select(vrt => vrt.Color).Distinct().ToArray();
 
+            List<Product> offers = new List<Product>();
+            List<Product> products = context.Products.AsNoTracking().ToList();
+
+            for (int i = 0; i < 4;)
+            {
+                var random = new Random(Guid.NewGuid().GetHashCode());
+                int index = random.Next(context.Products.Count());
+                Product newProduct = products[index];
+                bool included = false;
+                foreach (Product item in offers)
+                {
+                    if (item.Id == newProduct.Id) included = true;
+                }
+                if (!included)
+                {
+                    offers.Add(context.Products.First(prd => prd.Id == newProduct.Id));
+                    i++;
+                }
+            }
+            foreach(Product ofr in offers)
+            {
+                Variation ofrVariation = context.Variations.First(vrt => vrt.ProductId == ofr.Id);
+                ofr.Variations.Add(ofrVariation);
+            }
+
+            List<ProductImage> images = context.ProductImages.ToList();
+            foreach (Product offer in offers)
+                offer.ProductImages = images.Where(img => img.ProductId == offer.Id).ToList();
+
+            ViewBag.Offers = offers;
             ViewBag.Variation = variation;
             ViewBag.Product = product;
             ViewBag.Images = product.ProductImages;
             ViewBag.Sizes = sizes;
             ViewBag.Colors = colors;
+        }
+
+        private IActionResult IndexVariation(int vrtId)
+        {
+            Variation variation = context.Variations.First(vrt => vrt.Id == vrtId);
+            IndexBase(variation);
 
             return View();
         }
@@ -34,17 +71,7 @@ namespace ProjectBoots2.Controllers
         private IActionResult IndexChangeVariation(int productId, int size, string color)
         {
             Variation variation = context.Variations.First(vrt => vrt.ProductId == productId && vrt.Size == size && vrt.Color == color);
-            Product product = context.Products.First(prd => prd.Id == variation.ProductId);
-            product.ProductImages = context.ProductImages.Where(img => img.ProductId == product.Id).ToList();
-            List<Variation> variations = context.Variations.Where(vrt => vrt.ProductId == product.Id).ToList();
-            int?[] sizes = variations.Select(vrt => vrt.Size).Distinct().ToArray();
-            string?[] colors = variations.Select(vrt => vrt.Color).Distinct().ToArray();
-
-            ViewBag.Variation = variation;
-            ViewBag.Product = product;
-            ViewBag.Images = product.ProductImages;
-            ViewBag.Sizes = sizes;
-            ViewBag.Colors = colors;
+            IndexBase(variation);
 
             return View();
         }
